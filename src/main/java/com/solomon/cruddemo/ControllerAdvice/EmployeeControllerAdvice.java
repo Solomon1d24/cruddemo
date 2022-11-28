1d50fd6b1324ed5ccca89977226b466626877e89
Solomon1d24 <solo5698@connect.hku.hk>
2022-11-28 20:16:58 +0800

    RAFE-6 Commit of the controller advice for the employee

--- src/main/java/com/solomon/cruddemo/ControllerAdvice/EmployeeControllerAdvice.java
+++ src/main/java/com/solomon/cruddemo/ControllerAdvice/EmployeeControllerAdvice.java
@@ -1,7 +1,35 @@
 package com.solomon.cruddemo.ControllerAdvice;
 
+import com.solomon.cruddemo.Exception.EmployeeNotFoundException;
+import com.solomon.cruddemo.Response.EmployeeErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class EmployeeControllerAdvice {
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeErrorResponse> handleException(Exception exception) {
+
+        EmployeeErrorResponse employeeErrorResponse = new EmployeeErrorResponse();
+        employeeErrorResponse.setMessage(exception.getMessage());
+        employeeErrorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        employeeErrorResponse.setTimestamp(System.currentTimeMillis());
+        return new ResponseEntity<>(employeeErrorResponse, HttpStatus.BAD_REQUEST);
+
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeErrorResponse> handleException(EmployeeNotFoundException employeeNotFoundException) {
+
+        EmployeeErrorResponse employeeErrorResponse = new EmployeeErrorResponse();
+        employeeErrorResponse.setTimestamp(System.currentTimeMillis());
+        employeeErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        employeeErrorResponse.setMessage(employeeNotFoundException.getMessage());
+        return new ResponseEntity<>(employeeErrorResponse, HttpStatus.NOT_FOUND);
+
+    }
+
 }
